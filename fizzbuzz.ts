96dbdf2d9f32c0c805e3617d0424bc64d36aef80
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-01 16:55:49 +0100

    Fizzing and buzzing

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -3,9 +3,16 @@ function fizzbuzz() :void {
     console.log("Hello, World!");
 
     // Put your code here...
-
+    for (let i = 1; i < 101; i++){
+        if (!(i % 3)){
+            console.log("Fizz");
+        } else if (!(i % 5)){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
 }
 
 // Now, we run the main function:
 fizzbuzz();
-
