5e9cb3c191c4d2bd8322109e7e518bbccf95beae
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-01 17:10:19 +0100

    Actual Fizzing and Buzzing

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -4,13 +4,10 @@ function fizzbuzz() :void {
 
     // Put your code here...
     for (let i = 1; i < 101; i++){
-        if (!(i % 3)){
-            console.log("Fizz");
-        } else if (!(i % 5)){
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
+        if (!(i % 15)) console.log("FizzBuzz");
+        else if (!(i % 3)) console.log("Fizz");
+        else if (!(i % 5)) console.log("Buzz");
+        else console.log(i)
     }
 }
 
