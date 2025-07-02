f98d3d9d18b0734f0ea337710736d999d2034c97
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-02 15:40:56 +0100

    Refactor, Extension 2

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,44 +1,62 @@
 // This is our main function
 function fizzbuzz(): void {
-  console.log('Hello, World!');
+    console.log('Hello, World!');
 
   // Put your code here...
-  const readline = require('readline-sync');
-  let upto = readline.question("What number would you like to go up to? ");
-  let j = parseInt(upto)
+    type RuleFunction = (prev: string[]) => string[]
 
-  for (let i = 1; i < j + 1; i++) {
-    console.log(rules(i).join(''));
-  }
+    let initrules : [number, RuleFunction][]= [
+        [3, (res) => {
+            res.push('Fizz');
+            return res}],
+        [5, (res) => {
+            res.push('Buzz');
+            return res}],
+        [7, (res) => {
+            res.push('Bang');
+            return res}],
+        [11, (res) => {
+            res.length = 0;
+            res.push('Bong');
+            return res}],
+        [13, (res) => {
+            let position = res.findIndex(beforeB);
+            if (position === -1){
+                res.push('Fezz')
+            }
+            return res.splice(position, 0, 'Fezz');
+        }],
+        [17, (res)=>{return res.reverse()}]
+    ];
+
+    const readline = require('readline-sync');
+    let upto = readline.question("What number would you like to go up to? ");
+    let whichrules = readline.question("Which rules would you like to keep? (for 3, 5, 7, 11, 13, 17)\n \
+        Please type the numbers of rules you want to keep, separated by spaces. e.g. '3 5 11' ")
+    const keeprules = whichrules.split(" ").map(Number);
+
+    // let addrules = readline.question("How many rules would you like to add? (0 is a valid input)")
+    // if (parseInt(addrules)) {
+    //     for (let j = 1; j < parseInt(addrules) + 1; j++) {
+
+    //     }
+    // }
+
+    let ruleslist: [number, RuleFunction][] = initrules.filter((rule) => keeprules.includes(rule[0]))
+
+    for (let i = 1; i < parseInt(upto) + 1; i++) {
+        console.log(apply_rules(i, ruleslist).join(''));
+    }
 }
 
-function rules(i) {
+function apply_rules(i, ruleslist) {
     let res: string[] = [];
-    let ruleslist : [number,string,string][]= [
-        [3, 'Fizz', ''],
-        [5, 'Buzz', ''],
-        [7, 'Bang', ''],
-        [11, 'Bong', 'reset'],
-        [13, 'Fezz', 'before B'],
-        [17, '', 'reverse']
-    ];
 
     for (let rule of ruleslist) {
         if (i % rule[0] === 0) {
-            if (rule[2] === 'reset') {
-                res = [];
-            }
-            res.push(rule[1]);
-            if (rule[2] === 'before B') {
-                let position = res.findIndex(beforeB);
-                res.splice(position, 0, rule[1]);
-            }
-            if (rule[2] === 'reverse') {
-                res.reverse();
-            }
+            res = rule[1](res)
         }
     }
-
     if (!res.length) {
         res.push(i.toString());
     }
