0633b8673d1ddd5dc46445272713df6461267e89
Thea Sotthivej <Thea.Sotthivej@softwire.com>
2025-07-02 13:34:54 +0100

    Refactor

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -8,10 +8,22 @@ function fizzbuzz(): void {
   let j = parseInt(upto)
 
   for (let i = 1; i < j + 1; i++) {
-    const res: string[] = [];
+    const res2: string[] = rules(i);
+    console.log(res2.join(''));
+  }
+}
+
+function rules(i) {
+    let res: string[] = [];
+    // let ruleslist = [
+    //     [3, 'Fizz'],
+    //     [5, 'Buzz'],
+    //     [7, 'Bang'],
+    // ];
+
 
     if (i % 3 === 0) {
-      res.unshift('Fizz');
+      res.push('Fizz');
     }
     if (i % 5 === 0) {
       res.push('Buzz');
@@ -19,21 +31,25 @@ function fizzbuzz(): void {
     if (i % 7 === 0) {
       res.push('Bang');
     }
+    if (i % 11 === 0) {
+      res = [];
+      res.push('Bong');
+    }
+    if (i % 13 === 0) {
+      let position = res.findIndex(beforeB);
+      res.splice(position, 0, 'Fezz');
+    }
     if (i % 17 === 0){
       res.reverse()
     }
     if (!res.length) {
       res.push(i.toString());
     }
-    if (i % 11 === 0) {
-      res.length = 0;
-      res.push('Bong');
-    }
-    if (i % 13 === 0) {
-      res.unshift('Fezz');
-    }
-    console.log(res.join(''));
-  }
+    return res
+}
+
+function beforeB(value, index, array) {
+    return value.startsWith("B")
 }
 
 // Now, we run the main function:
